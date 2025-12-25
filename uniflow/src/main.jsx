0240eb1e6edf26637611e8cbb715fde321484479
Shubham Bhardwaj <bhardwajshubham0777@gmail.com>
2025-12-26 03:26:27 +0530

    final 3 am update

--- uniflow/src/main.jsx
+++ uniflow/src/main.jsx
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { ThemeProvider } from './context/ThemeContext';
-import { AuthProvider } from './context/AuthContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom' // 👈 Critical Import
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </AuthProvider>
+    <BrowserRouter> {/* 👈 The Router must wrap everything */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-);
+)
