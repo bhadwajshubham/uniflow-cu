129c0be26e848a1f204f426128726f257c3f1a6e
Shubham Bhardwaj <bhardwajshubham0777@gmail.com>
2025-12-26 21:50:29 +0530

    Upload new featues in files

--- uniflow/src/App.jsx
+++ uniflow/src/App.jsx
@@ -5,16 +5,14 @@ import { AuthProvider } from './context/AuthContext';
 import { ThemeProvider } from './context/ThemeContext';
 import ProtectedRoute from './components/layout/ProtectedRoute';
 
+// 1. Auth Components
 import LoginPage from './features/auth/components/LoginPage';
-import HelpPage from './features/auth/components/HelpPage'; 
 
-// Events
+// 2. Event Components (Note: Based on your file structure, these are all in events/components)
 import EventsPage from './features/events/components/EventsPage';
-// 👇 NEW IMPORT
-import EventDetailsPage from './features/events/components/EventDetailsPage'; 
+import EventDetailsPage from './features/events/components/EventDetailsPage'; // 👈 The new page
 import MyTicketsPage from './features/events/components/MyTicketsPage';
-
-// Admin
+import HelpPage from './features/events/components/HelpPage'; // 👈 FIXED PATH (was causing error)
 import AdminDashboard from './features/events/components/AdminDashboard';
 import ScanPage from './features/events/components/ScanPage';
 
@@ -25,16 +23,18 @@ const App = () => {
         <div className="min-h-screen bg-zinc-50 dark:bg-black text-zinc-900 dark:text-white transition-colors duration-300">
           <Navbar />
           <Routes>
-            {/* Public */}
+            {/* Public Routes */}
             <Route path="/login" element={<LoginPage />} />
+            
+            {/* Help Page (Fixed Path) */}
             <Route path="/help" element={<HelpPage />} />
+            
+            {/* Events Routes */}
             <Route path="/" element={<EventsPage />} />
             <Route path="/events" element={<EventsPage />} />
-            
-            {/* 👇 THIS FIXES THE BLUE SCREEN */}
-            <Route path="/events/:id" element={<EventDetailsPage />} /> 
+            <Route path="/events/:id" element={<EventDetailsPage />} /> {/* 👈 Fixes Blue Screen */}
 
-            {/* Protected Student */}
+            {/* Protected Student Routes */}
             <Route 
               path="/my-tickets" 
               element={
@@ -44,7 +44,7 @@ const App = () => {
               } 
             />
 
-            {/* Protected Admin */}
+            {/* Protected Admin Routes */}
             <Route 
               path="/admin" 
               element={
